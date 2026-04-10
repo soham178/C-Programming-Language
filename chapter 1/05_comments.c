ee752d68472157cc7c031bb5590623de62a78682
soham178 <sohamsarkar552@gmail.com>
2026-04-10 11:26:38 +0530

    add comments

---
+++ chapter 1/05_comments.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    
+    // I am writing this just for fun. This line doesn't do anything
+
+    printf("Hello World");
+
+    // return 0 is returning 0 to the os
+
+    return 0; // this is return
+
+    /*
+    this is a multiline comment
+    Program: 05_comments.c
+    Author: Soham
+    Date: 4/10/2025
+    */
+
+} 
