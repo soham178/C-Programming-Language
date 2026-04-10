679973df0e6096ad38cc689727e003b39e13766d
soham178 <sohamsarkar552@gmail.com>
2026-04-10 11:06:28 +0530

    add variable rules

---
+++ chapter 1/02_variables_rules.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int harry;
+    int Harry;
+    int harry_good;
+    return 0;
+} 
+
+/*
+1. first character must be an alphabet or underscore.
+2. no commas, blanks are allowed.
+3. no special symbol other than underscore allowed.
+4. variable names are case sensetive.
+*/
