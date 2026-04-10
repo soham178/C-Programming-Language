b7e546cbed699c30076176546bea41e113aa58b3
soham178 <sohamsarkar552@gmail.com>
2026-04-10 17:17:48 +0530

    add variables in ch2

---
+++ chapter 2/01_variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int i = 10; // declare and initialize 'i' with 10
+    int j = i;
+    int a = 2, b = 3, c = 4, d = 5;
+    // %d is called format specifier
+    // %d is for int, %f for float and %c for char
+    printf("The value of i is %d and value of j is %d\n", i,j);
+    printf("The value of a is %d and value of b is %d\n", a,b);
+    printf("The value of c is %d and value of d is %d\n", c,d);
+    return 0;
+} 
